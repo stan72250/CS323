38bb7466a8b77306280c569af4c07fcf2bd55eb8
Stanley Lin <54613937+stan72250@users.noreply.github.com>
2019-09-12 11:37:08 -0400

    Update and rename Project1.java to Driver.java

---
+++ Project 1/Driver.java
@@ -0,0 +1,29 @@
+//CS323 Project 1
+//Authors: Stanley Lin & Yajie Zhang
+
+import java.util.*;
+
+public class Driver{
+  public static void main(String args[]){
+    int n;
+    
+    
+    Random rand = new Random();
+    Scanner input = new Scanner(System.in);
+    
+    System.out.print("Enter a number of integers to generate: ");
+    n = input.nextInt();
+    int[] arr = new int[n];
+    
+    System.out.println();
+    for(int i = 0;i < n; i++){
+      arr[i] = rand.nextInt(100) + 1;
+    }
+    
+    System.out.println("Unsorted Integers: ");
+    for(int i = 0; i < arr.length; i++){
+      System.out.print(arr[i] + " ");
+    }
+    System.out.println();
+  }
+}
