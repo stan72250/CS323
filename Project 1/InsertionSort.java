6bfc8e2c1cfca6165c06a2770b58d1ad9ef42395
Stanley Lin <54613937+stan72250@users.noreply.github.com>
2019-09-12 11:43:51 -0400

    Update InsertionSort.java
    remove printArray method

--- Project 1/InsertionSort.java
+++ Project 1/InsertionSort.java
@@ -12,12 +12,4 @@ class InsertionSort{
 			arr[j + 1] = key;
 		}
 	}
-	
-	static void printArray(int arr[]) {
-		int n = arr.length;
-		for(int i = 0; i < n; i++) {
-			System.out.print(arr[i] + " ");
-		}
-		System.out.println();
-	}
 }
