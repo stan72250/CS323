22cce0a1d37d361c350d259746817f73b4c75a9c
Stanley Lin <54613937+stan72250@users.noreply.github.com>
2019-09-29 19:52:54 -0400

    Update MedianOfMedians.java

--- Project 1/MedianOfMedians.java
+++ Project 1/MedianOfMedians.java
@@ -12,4 +12,21 @@ class MedianOfMedians {
   public void findMedian(){
     
   }
+  
+  private static int partition(int[] arr, int l, int r) {
+		int x = arr[r], i = l;
+		for(int j = l; j <= r - 1; j++) {
+			if(arr[j] <= x) {
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+				i++;
+			}
+		}
+		int temp = arr[i];
+		arr[i] = arr[r];
+		arr[r] = temp;
+		
+		return i;
+	}
 }
