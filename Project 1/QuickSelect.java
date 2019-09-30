898184aaaa317797b9f4ab0cfa60924320665e02
Stanley Lin <54613937+stan72250@users.noreply.github.com>
2019-09-30 16:29:56 -0400

    Update QuickSelect.java

--- Project 1/QuickSelect.java
+++ Project 1/QuickSelect.java
@@ -1,53 +1,53 @@
 import java.util.*;
 class QuickSelect {
 	private int[] arr;
-	private int l;
-	private int r;
 	private int k;
+	private static int count = 0;
 	
 	public QuickSelect(int[] a, int n){
 		arr = a;
-		l = 0;
-		r = arr.length - 1;
 		k = n;
 	}
 	
 	public void run() {
-		kthSmallest(arr, l, r, k);
+		System.out.println(kthSmallest(arr, k));
+		System.out.println("Number of Comparisons: " + numOfComparisons());
 	}
 	
-	private static int partition(int[] arr, int l, int r) {
-		int x = arr[r], i = l;
-		for(int j = l; j <= r - 1; j++) {
-			if(arr[j] <= x) {
-				int temp = arr[i];
-				arr[i] = arr[j];
-				arr[j] = temp;
-				i++;
+	private int kthSmallest(int[] arr, int k) {
+		int l = 0;
+		int r = arr.length - 1;
+		
+		while(l < r){
+			int pivot = (l + r) / 2;
+			int pivotValue = arr[pivot];
+			int storage = l;
+			
+			arr[pivot] = arr[r];
+			arr[r] = pivotValue;
+			for(int i = l; i < r; i++) {
+				if(arr[i] < pivotValue) {
+					int temp = arr[storage];
+					arr[storage] = arr[i];
+					arr[i] = temp;
+					storage++;
+					count++;
+				}
+			}
+			arr[r] = arr[storage];
+			arr[storage] = pivotValue;
+			
+			if(storage < k) {
+				l = storage + 1;
+			}
+			else {
+				r = storage;
 			}
 		}
-		int temp = arr[i];
-		arr[i] = arr[r];
-		arr[r] = temp;
-		
-		return i;
+		return arr[k - 1];
 	}
 	
-	private static int kthSmallest(int[] arr, int l, int r, int k) {
-		if(k > 0 && k <= r - 1 + 1) {
-			int pos = partition(arr, l, r);
-			
-			if(pos - 1 == k - 1) {
-				return arr[pos];
-			}
-			
-			if(pos - 1 > k - 1) {
-				return kthSmallest(arr, l, pos - 1, k);
-			}
-			
-			return kthSmallest(arr, pos + 1, r, k - pos + l - 1);
-		}
-		
-		return Integer.MAX_VALUE;
+	private static int numOfComparisons() {
+		return count;
 	}
 }
