4479dd3694b9e51ef16ed2d7208294bff973cade
Stanley Lin <54613937+stan72250@users.noreply.github.com>
2019-09-30 16:09:09 -0400

    Update SelectionSort.java

--- Project 1/SelectionSort.java
+++ Project 1/SelectionSort.java
@@ -2,6 +2,7 @@
 class SelectionSort {
 	private int[] arr;
 	private int k;
+	private static int count = 0;
 	
 	public SelectionSort(int[] a, int n){
 		arr = a;
@@ -10,24 +11,42 @@ class SelectionSort {
 	
 	public void run() {
 		SSort(arr, k);
+		printArray(arr);
+		System.out.println("Median Element: " + SSort(arr, k));
+		System.out.println("Number of Comparisons: " + numOfComparisons());
 	}
 	
 	private int SSort(int arr[], int k) {
 		int n = arr.length;
 		
-		for(int i = 1; i <= k; i++) {
+		for(int i = 0; i < n - 1; i++) {
 			int min_idx = i;
-			int min_value = arr[i];
+	
 			for(int j = i + 1; j < n; j++) {
 				if(arr[j] < arr[min_idx]) {
 					min_idx = j;
-					min_value = arr[j];
-					int temp = arr[min_idx];
-					arr[min_idx] = arr[i];
-					arr[i] = temp;
-				}
-			}			
+					count++;
+				}	
+			}	
+			
+			int temp = arr[min_idx];
+			arr[min_idx] = arr[i];
+			arr[i] = temp;
+										
 		}
-		return arr[k];
+		return arr[k - 1];
+	}
+	
+	private static void printArray(int[] arr) {
+		int n = arr.length;
+		System.out.println("Sorted Array: ");
+		for(int i = 0; i < n; ++i) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+	}
+	
+	private static int numOfComparisons() {
+		return count;
 	}
 }
