4d8028c040709136dca3ffe90ce379aadf363b4c
Stanley Lin <54613937+stan72250@users.noreply.github.com>
2019-10-02 20:59:24 -0400

    Update HeapSort.java

--- Project 1/HeapSort.java
+++ Project 1/HeapSort.java
@@ -1,40 +1,66 @@
-
+/*Authors: Stanley Lin & Yajie Zhang
+* References (URLs): https://www.cs.wcupa.edu/rkline/ds/heaps.html, http://pages.cs.wisc.edu/~paton/readings/Old/fall01/HEAP-SORT, 
+* https://rosettacode.org/wiki/Sorting_algorithms/Heapsort#J
+*
+*/
 class HeapSort {
+	private int[] arr;
+	private int k;
+	private static long count = 0;
+	
+	public HeapSort(int[] a, int n) {
+		arr = a;
+		k = n;
+	}
+	
+	public void run(){
+		count = 0;
+		sort(arr);
+		System.out.println("Median Element: " + kthElement());
+		System.out.println("Number of Comparisons: " + numOfComparisons());
+	}
+	
 	private void sort(int arr[]) {
-		int n = arr.length;
-		
-		for(int i = n / 2 - 1; i >= 0; i--) {
-			heapify(arr, n, i);
+		for(int i = arr.length / 2; i >=0; i--) {
+			percolateDown(arr, i, arr.length);
 		}
-		
-		for(int i = n - 1; i >= 0; i--) {
+		for(int i = arr.length - 1; i > 0; i--) {
 			int temp = arr[0];
 			arr[0] = arr[i];
 			arr[i] = temp;
-			
-			heapify(arr, i, 0);
+			percolateDown(arr, 0, i);
 		}
 	}
 	
-	private void heapify(int arr[], int n, int i) {
-		int largest = i;
-		int l = 2 * i + 1;
-		int r = 2 * i + 2;
+	private static int leftChild(int i) {
+		return 2 * i + 1;
+	}
+	
+	private static void percolateDown(int[] arr, int i, int n) {
+		int child, temp;
 		
-		if(l < n && arr[l] > arr[largest]) {
-			largest = l;
+		for(temp = arr[i]; leftChild(i) < n; i = child) {
+			child = leftChild(i);
+			if(child != n - 1 && arr[child] < arr[child + 1]) {
+				child++;
+				count++;
+			}
+			if(temp < arr[child]) {
+				arr[i] = arr[child];
+				count++;
+			}
+			else {
+				break;
+			}
 		}
-		
-		if(r < n && arr[r] > arr[largest]) {
-			largest = r;
-		}
-		
-		if(largest != i) {
-			int swap = arr[i];
-			arr[i] = arr[largest];
-			arr[largest] = swap;
-			
-			heapify(arr, n, largest);
-		}
+		arr[i] = temp;
+	}
+	
+	private int kthElement() {
+		return arr[k - 1];
+	}
+	
+	private static long numOfComparisons(){
+		return count;
 	}
 }
