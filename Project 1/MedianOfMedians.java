a494dced485c72b9d8ed4d83610951de0ac34fc5
Stanley Lin <54613937+stan72250@users.noreply.github.com>
2019-10-02 21:01:53 -0400

    Update MedianOfMedians.java

--- Project 1/MedianOfMedians.java
+++ Project 1/MedianOfMedians.java
@@ -7,28 +7,23 @@ class MedianOfMedians {
 	private int l;
 	private int r;
 	private int k;
-	private int count = 0;
+	private static long count = 0;
 	
 	public MedianOfMedians(int[] a, int n) {
 		arr = a;
-		k = n;
 		l = 0;
-		r = arr.length - 1;
-		
+		r = a.length - 1;
+		k = n;
 	}
 	
 	public void run() {
+		count = 0;
 		System.out.println("Median Element: " + kthSmallest(arr, l, r, k));
 		System.out.println("Number of Comparisons: " + numOfComparisons());
 	}
 	
-	private int findMedian(int[] a, int i, int n) {
-		if(i <= n) {
-			Arrays.sort(a, i, n);
-		}
-		else {
-			Arrays.sort(a, n, i);
-		}
+	private static int findMedian(int[] a, int n) {
+		Arrays.sort(a);
 		return a[n/2];
 	}
 	
@@ -39,65 +34,65 @@ class MedianOfMedians {
 			int i;
 			
 			int[] median = new int[(n + 4) / 5];
-			for(i = 0; i < n / 5; i++) {
-				median[i] = findMedian(a, l + i * 5, 5);
+			for(i = 0; i < median.length - 1; i++) {
+				median[i] = findMedian(Arrays.copyOfRange(a, 5 * i + l, 5 * i + l + 4), 5);
 			}
-			if(i * 5 < n) {
-				median[i] = findMedian(a, l + i * 5, n % 5);
+			if(n % 5 == 0) {
+				median[i] = findMedian(Arrays.copyOfRange(a, 5 * i + l, 5 * i + l + 4), 5);
+			}
+			else {
+				median[i] = findMedian(Arrays.copyOfRange(a, 5 * i + l, 5 * i + l + (n % 5)), n % 5);
 				i++;
 			}
 			
-			int medOfMed = (i == 1)? median[i - 1]:
-								kthSmallest(median, 0, i - 1, i / 2);
+			int medOfMed = i == 1 ? median[i - 1]
+					: kthSmallest(median, 0, i - 1, i / 2);
 			
-			int pos = partition(a, l, r, medOfMed);
+			int par = partition(a, l, r, medOfMed);
 			
-			if(pos - l == k - 1) {
-				count++;
-				return a[pos];
+			if(par - l == k - 1) {
+				return a[par];
 			}
-			if(pos - l > k - 1) {
-				count++;
-				return kthSmallest(a, l , pos - 1, k);
+			
+			if(par - l > k - 1) {
+				return kthSmallest(a, l, par - 1, k);
 			}
-			count++;
-		return kthSmallest(a, pos + 1, r, k - pos + l - 1);
+			return kthSmallest(a, par + 1, r, k - (par + 1) + l);
 		}
 		return Integer.MAX_VALUE;
 	}
 	
-	private static int[] swap(int []arr, int i, int j) { 
+	private static void swap(int []arr, int i, int j) { 
 	    int temp = arr[i]; 
 	    arr[i] = arr[j]; 
-	    arr[j] = temp; 
-	    return arr; 
+	    arr[j] = temp; 	    
 	} 
 	  
-	// It searches for x in arr[l..r], and  
-	// partitions the array around x. 
-	private int partition(int arr[], int l, int r, int x) { 
-	    // Search for x in arr[l..r] and move it to end 
-	    int i; 
-	    for (i = l; i < r; i++) 
-	        if (arr[i] == x) 
-	        break; 
-	    swap(arr, i, r); 
-	  
-	    // Standard partition algorithm 
-	    i = l; 
-	    for (int j = l; j <= r - 1; j++) 
-	    { 
-	        if (arr[j] <= x) 
-	        { 
-	            swap(arr, i, j); 
-	            i++; 
-	        } 
-	    } 
-	    swap(arr, i, r); 
-	    return i; 
+
+	private static int partition(int arr[], int l, int r, int pivot) {      
+	    for(int i = 0; i < arr.length; i++) {
+	    	if(arr[i] == pivot) {
+	    		count++;
+	    		swap(arr, i, r);
+	    		break;
+	    	}
+	    }
+	    int index = l - 1;
+	    int i = l;
+	    while(i < r) {
+	    	if(arr[i] < pivot) {
+	    		count++;
+	    		index++;
+	    		swap(arr, i, index);
+	    	}
+	    	i++;
+	    }
+	    index++;
+	    swap(arr, index, r);
+	    return index; 
 	} 
 	
-	private int numOfComparisons() {
+	private static long numOfComparisons() {
 		return count;
 	}
 }
